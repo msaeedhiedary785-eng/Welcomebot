18532453689a8d8e881390f223cb49133e956a7a
msaeedhiedary785-eng <msaeedhiedary785@gmail.com>
2026-09-11 00:22:37 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -19,9 +19,20 @@ def keep_alive():
     t = Thread(target=run_web)
     t.start()
 
-@bot.message_handler(func=lambda message: 'سلام' in message.text)
+# پاسخ به سلام
+@bot.message_handler(func=lambda message: message.text and 'سلام' in message.text)
 def send_welcome(message):
-    bot.reply_to(message, "سلام، خوبین ؟\nبه مشهد استار خوش اومدی 💫\nامیدوارم حال دلت خوب باشه 💞")
+    bot.send_message(message.chat.id, "سلام، خوبین ؟\nبه مشهد استار خوش اومدی 💫\nامیدوارم حال دلت خوب باشه 💞", reply_to_message_id=message.message_id)
+
+# پاسخ به خداحافظ و بای
+@bot.message_handler(func=lambda message: message.text and ('خداحافظ' in message.text or 'بای' in message.text))
+def send_goodbye(message):
+    bot.send_message(message.chat.id, "چه زود داری میری 🥺", reply_to_message_id=message.message_id)
+
+# پاسخ به لفت یا لف
+@bot.message_handler(func=lambda message: message.text and ('لفت' in message.text or 'لف' in message.text))
+def send_left(message):
+    bot.send_message(message.chat.id, "خیلی بدی کجا میری منو تنها میزاری؟ 💔", reply_to_message_id=message.message_id)
 
 if __name__ == '__main__':
     keep_alive()
