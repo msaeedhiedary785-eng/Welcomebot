a8db0ae9337cf6ccc5e8593abc7989e583b05582
msaeedhiedary785-eng <msaeedhiedary785@gmail.com>
2026-09-10 23:21:30 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,38 +1,32 @@
 import os
 from flask import Flask
+from threading import Thread
 import telebot
 
-TOKEN = os.environ.get('BOT_TOKEN', 'YOUR_BOT_TOKEN')
+TOKEN = '8628828031:AAFK0mSv7Sp2caHb9dmM02N3ITTqIfqVu5g'
 bot = telebot.TeleBot(TOKEN)
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is active!"
+    return "Bot is active and running!"
 
-# پاسخ به دستور start/
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    welcome_text = "سلام خوبی\nبه مشهد استار خوش اومدی امیدوارم حال دلت خوب باشه"
-    bot.reply_to(message, welcome_text)
-
-# حساسیت به کلمه سلام در متن پیام‌ها
-@bot.message_handler(func=lambda message: 'سلام' in message.text.lower())
-def handle_hello(message):
-    reply_text = "سلام خوبی\nبه مشهد استار خوش اومدی امیدوارم حال دلت خوب باشه"
-    bot.reply_to(message, reply_text)
-
-# پاسخ به بقیه پیام‌ها
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-def run_flask():
+def run_web():
     app.run(host='0.0.0.0', port=8080)
 
-if __name__ == '__main__':
-    import threading
-    t = threading.Thread(target=run_flask)
+def keep_alive():
+    t = Thread(target=run_web)
     t.start()
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! ربات شما با موفقیت روشن شد و آماده به کار است. 🎉")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "پیام شما دریافت شد: " + message.text)
+
+if __name__ == '__main__':
+    keep_alive()
     bot.infinity_polling()
