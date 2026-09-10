9ebdeb978be0583d5addf79e783399e54488e67a
msaeedhiedary785-eng <msaeedhiedary785@gmail.com>
2026-09-11 01:57:30 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from threading import Thread
 import telebot
-import re
 
 TOKEN = '8628828031:AAFK0mSv7Sp2caHb9dmM02N3ITTqIfqVu5g'
 bot = telebot.TeleBot(TOKEN)
@@ -20,18 +19,18 @@ def keep_alive():
     t = Thread(target=run_web)
     t.start()
 
-# پاسخ به سلام (فقط کلمه‌ی کامل سلام)
-@bot.message_handler(func=lambda message: message.text and bool(re.search(r'\bسلام\b', message.text)))
+# پاسخ به سلام
+@bot.message_handler(func=lambda message: message.text and 'سلام' in message.text)
 def send_welcome(message):
     bot.send_message(message.chat.id, "سلام، خوبین ؟\nبه مشهد استار خوش اومدی 💫\nامیدوارم حال دلت خوب باشه 💞", reply_to_message_id=message.message_id)
 
-# پاسخ به خداحافظ، خدافظ یا بای (فقط کلمات کامل)
-@bot.message_handler(func=lambda message: message.text and bool(re.search(r'\b(خداحافظ|خدافظ|بای)\b', message.text)))
+# پاسخ به خداحافظ، خدافظ یا بای
+@bot.message_handler(func=lambda message: message.text and ('خداحافظ' in message.text or 'خدافظ' in message.text or 'بای' in message.text))
 def send_goodbye(message):
     bot.send_message(message.chat.id, "چه زود داری میری 🥺", reply_to_message_id=message.message_id)
 
-# پاسخ به لف یا لفت (فقط کلمات کامل)
-@bot.message_handler(func=lambda message: message.text and bool(re.search(r'\b(لف|لفت)\b', message.text)))
+# پاسخ به لف یا لفت
+@bot.message_handler(func=lambda message: message.text and ('لفت' in message.text or 'لف' in message.text))
 def send_left(message):
     bot.send_message(message.chat.id, "خیلی بدی کجا میری منو تنها میزاری؟ 💔", reply_to_message_id=message.message_id)
 
